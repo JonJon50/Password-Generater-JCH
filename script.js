2cb0c184801e0e8fafe319a93afd1270c079a08b
John Hagens <ginuwine104@gmail.com>
2022-12-22 15:23:49 -0500

    add characters

--- script.js
+++ script.js
@@ -1,14 +1,96 @@
-// Assignment Code
+// Array of special chracters ti ne included in passwpord
+var specialCharacters = [
+ '!',
+ '@',
+ '#',
+ '$',
+ '%',
+ '^',
+ '&',
+ '*',
+ '(',
+ ')',
+ '-',
+ '_',
+ '=',
+ '+',
+ '`',
+ '~',
+ '[',
+ '{',
+ '}',
+ ']',
+ ':',
+ '?',
+ '/',
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '8', '9',]
+
+// Array of lowerCase characters to be included in password
+var lowerCaseCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+];
+
+// Array of upperCase characters to be included in password
+var upperCase = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+
+
+
+
+// Assignment code here
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
